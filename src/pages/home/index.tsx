059d03481ed71f7b102bc34c875eb3c480e19297
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-09-17 01:44:27 +0900

    App.tsx에 라우터 설정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { Slider, Typography } from "@mui/material";
+
+function Home() {
   return (
     <>
-      <div>홈</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Slider defaultValue={30} />
+      <Slider defaultValue={30} className="text-teal-600" />
+      <Typography variant="h1" color="root.error">
+        내용
+      </Typography>
+      <span color="root.error">
+        일단 span 코드에는 tailwind css를 사용해야 함
+      </span>
+      <div className="text-teal-600">이것 처럼</div>
     </>
   );
 }
+
+export default Home;
