a31031392db78fd8d374c56a2d812894504a10df
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-09-17 01:41:16 +0900

    테스트용 home 생성

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,13 +1,14 @@
 import { RouteObject } from "react-router-dom";
 import DefaultLayout from "../layout";
 import PathConstants from "./path";
+import Home from "../pages/home";
 
 export const routes: RouteObject[] = [
   {
     element: <DefaultLayout />,
     // 에러 엘리먼트 추가 필요
     children: [
-      { path: PathConstants.Home, element: <div> 홈 </div> },
+      { path: PathConstants.Home, element: <Home /> },
       {
         children: [
           {
