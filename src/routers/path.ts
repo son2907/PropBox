25740c47338e6bc261b9284f597791123529269a
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-10-17 16:04:08 +0900

    메뉴 라우터 작성

--- src/routers/path.ts
+++ src/routers/path.ts
@@ -2,6 +2,29 @@ const PathConstants = {
   Home: "/",
   Login: "/login",
   Test: "/test",
+  Call: {
+    Consultation: "/call/consultation",
+    Analysis: "/call/analysis",
+    Management: "/call/management",
+    Information: "/call/information",
+  },
+  Message: {
+    Auto: "/message/auto",
+    Bulk: "/message/bulk",
+    Declaration: "/message/declartion", // 오타 수정: declartion -> declaration
+    Reject: "/message/reject",
+  },
+  Customer: {
+    Registration: "/customer/registration",
+  },
+  System: {
+    Solution: "/system/solution",
+    User: "/system/user",
+    Member: "/system/member",
+    Local: "/system/local",
+    LocalMember: "/system/localmember",
+    Auth: "/system/auth",
+  },
 };
 
 export default PathConstants;
