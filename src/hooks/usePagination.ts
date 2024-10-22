aa47f6ef90378ce9e59088cd6bf59cf0a5bf9f12
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-10-23 04:02:45 +0900

    체크박스 항목 리스트 컴포넌트

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -4,12 +4,12 @@ export const usePagination = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
   // 페이지 변경 함수
-  const onChange = (event: React.ChangeEvent<unknown>, newPage: number) => {
+  const onChangePage = (event: React.ChangeEvent<unknown>, newPage: number) => {
     setCurrentPage(newPage); // 새로운 페이지 번호로 상태 업데이트
   };
 
   return {
     currentPage, // 현재 페이지 반환
-    onChange, // 페이지 변경 함수 반환
+    onChangePage, // 페이지 변경 함수 반환
   };
 };
