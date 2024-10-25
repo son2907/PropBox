a2277de5202142a1af8d1453659d21d42674604d
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-10-26 06:53:47 +0900

    캘린더 스타일

--- src/components/Calendar/RangeCalendar.tsx
+++ src/components/Calendar/RangeCalendar.tsx
@@ -2,10 +2,21 @@ import { ko } from "date-fns/locale";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./RangeCalendar.css"; // 스타일 파일 import
 
 export default function RangeCalendar() {
   const [startDate, setStartDate] = useState<Date | undefined>(new Date());
   const [endDate, setEndDate] = useState<Date | undefined>(new Date());
+
+  // 날짜별 클래스를 설정
+  const dayClassName = (date: Date) => {
+    if (startDate && endDate) {
+      if (date.getTime() === startDate.getTime()) return "start-date";
+      if (date.getTime() === endDate.getTime()) return "end-date";
+      if (date > startDate && date < endDate) return "in-range";
+    }
+    return "";
+  };
 
   return (
     <DatePicker
@@ -18,10 +29,12 @@ export default function RangeCalendar() {
         setEndDate(end);
       }}
       locale={ko}
-      dateFormat="yyyy.MM.dd" // 날짜 형태
-      shouldCloseOnSelect={false} // 두 날짜를 선택 후 datepicker가 닫힘
-      minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
-      maxDate={new Date()} // maxDate 이후 날짜 선택 불가
+      dateFormat="yyyy.MM.dd"
+      shouldCloseOnSelect={false}
+      minDate={new Date("2000-01-01")}
+      maxDate={new Date()}
+      dayClassName={dayClassName} // 날짜별 클래스 적용
+      renderDayContents={(day) => <span style={{ zIndex: 1000 }}>{day}</span>}
       renderCustomHeader={({
         date,
         changeYear,
@@ -36,9 +49,14 @@ export default function RangeCalendar() {
             margin: 10,
             display: "flex",
             justifyContent: "center",
+            backgroundColor: "white",
           }}
         >
-          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+          <button
+            style={{ backgroundColor: "transparent", border: "none" }}
+            onClick={decreaseMonth}
+            disabled={prevMonthButtonDisabled}
+          >
             {"<"}
           </button>
           <select
@@ -65,7 +83,11 @@ export default function RangeCalendar() {
               </option>
             ))}
           </select>
-          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+          <button
+            style={{ backgroundColor: "transparent", border: "none" }}
+            onClick={increaseMonth}
+            disabled={nextMonthButtonDisabled}
+          >
             {">"}
           </button>
         </div>
