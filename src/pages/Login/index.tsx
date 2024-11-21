8089b87b9d44a3da722406abad78eb4e4fa27f8c
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2024-11-21 16:33:57 +0900

    로그인 컴포넌트 수정

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,8 +2,9 @@ import { useRef } from "react";
 import Logo from "../../assets/svg/logo.svg";
 import BasicInput from "../../components/Input/BasicInput";
 import PasswordInput from "../../components/Input/PasswordInput";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import BlackButton from "../../components/Button/BlackButton";
+import CenteredBox from "../../components/Box/CenteredBox";
 
 export default function Login() {
   const idRef = useRef<HTMLInputElement | null>(null);
@@ -13,34 +14,24 @@ export default function Login() {
     console.log(idRef.current?.value, passwordRef.current?.value);
   };
   return (
-    <Box
-      style={{
-        display: "flex",
-        flexFlow: "column",
-        justifyContent: "center",
-        width: "250px",
-        gap: 6,
-      }}
-    >
+    <Stack width={"300px"} gap={1} justifyContent={"center"}>
       <Box>
         <img src={Logo} alt="logo" width={"100%"} />
       </Box>
-      <Box marginBottom={1.5}>
-        <Typography color="primary.dark" marginBottom={1}>
+      <CenteredBox gap={1}>
+        <Typography color="primary.dark" width={"64px"}>
           아이디
         </Typography>
-        <BasicInput ref={idRef} placeholder="아이디를 입력하세요" />
-      </Box>
-      <Box marginBottom={1.5}>
-        <Typography color="primary.dark" marginBottom={1}>
-          비밀번호
-        </Typography>
+        <BasicInput ref={idRef} placeholder="아이디를 입력하세요" fullWidth />
+      </CenteredBox>
+      <CenteredBox gap={1}>
+        <Typography color="primary.dark">비밀번호</Typography>
         <PasswordInput ref={passwordRef} />
-        {/* api 완성 후 수정 */}
-        <Typography marginTop={1} color="error.main">
-          사용자 정보가 잘못되었습니다.
-        </Typography>
-      </Box>
+      </CenteredBox>
+      {/* api 완성 후 수정 */}
+      <Typography marginTop={1} color="error.main">
+        사용자 정보가 잘못되었습니다.
+      </Typography>
       <Box style={{ display: "flex" }}>
         <Typography className="underline">아이디 기억하기</Typography>
         <input type="checkbox" />
@@ -49,6 +40,6 @@ export default function Login() {
       <BlackButton onClick={fu_Login}>로그인</BlackButton>
 
       <Typography color="pageTab.tabIcon">문의 전화번호 : 1661-8050</Typography>
-    </Box>
+    </Stack>
   );
 }
