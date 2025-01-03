93535e1a3a7662ebc18dc13863f66bffe159b1f2
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-01-03 23:30:00 +0900

    현장 변경 시 새로곷침

--- src/components/layout/Appbar.tsx
+++ src/components/layout/Appbar.tsx
@@ -6,6 +6,8 @@ import useSelect from "../../hooks/useSelect";
 import UserInfo from "./UserInfo";
 import { useSiteList } from "../../api/siteList";
 import getItemByStorageOne from "../../utils/getItemByStorageOne";
+import { useEffect } from "react";
+import { filterDataByValues } from "../../utils/filterDataByValues";
 
 // AppBarArea 컴포넌트: 앱바 영역
 const AppBarArea = styled(Box)(({ theme }) => ({
@@ -33,6 +35,38 @@ export default function Appbar() {
     "sptNm", // 현장명
     sptNo
   );
+
+  useEffect(() => {
+    // 현장 선택 데이터가 바뀌면
+    // data에서 해당 현장 정보를 가져와서
+    // storage에 저장된 값을 바꾸어야 함
+    // 그리고 사이트를 리셋함
+    console.log("현장:", selectValue);
+    console.log("데이터 리스트:", data?.data.contents);
+    console.log(sptNo == selectValue);
+    console.log(
+      filterDataByValues({
+        data: data?.data.contents,
+        key: "sptNo",
+        values: [selectValue],
+      })
+    );
+
+    if (sptNo == selectValue) return;
+
+    localStorage.setItem(
+      "selectedSite",
+      JSON.stringify(
+        filterDataByValues({
+          data: data?.data.contents,
+          key: "sptNo",
+          values: [selectValue],
+        })
+      )
+    );
+    // 위의 동작을 끝내고 나서 리프래시
+    window.location.reload();
+  }, [selectValue]);
 
   return (
     <AppBarArea>
