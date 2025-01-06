27c6030a179422afa499f7b2a8c9bfa975960b53
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-01-07 01:11:17 +0900

    불필요 log 제거

--- src/pages/Notice/NoticeDetail.tsx
+++ src/pages/Notice/NoticeDetail.tsx
@@ -14,10 +14,7 @@ import BasicInput from "../../components/Input/BasicInput";
 import Calendar from "../../components/Calendar/Calendar";
 import { noticeDetailType, useNoticeDetail } from "../../api/noticeDetail";
 
-
-
 export default function NoticeDetail() {
-
   //팝업 페이지에서 id를 가져오려면 window.location.search를 사용하여 파라미터를 파싱
   const queryParams = new URLSearchParams(window.location.search);
   const id = queryParams.get("id");
@@ -25,41 +22,35 @@ export default function NoticeDetail() {
 
   //공지사항 상세 가져오기
   const { isSuccess, data } = useNoticeDetail(id);
-  const [noticeDetail, setNoticeDetail] = useState<noticeDetailType | null>(null);
+  const [noticeDetail, setNoticeDetail] = useState<noticeDetailType | null>(
+    null
+  );
   const [title, setTitle] = useState("");
 
   useEffect(() => {
     console.log("data확인", data);
-    if(data?.data.contents) {
+    if (data?.data.contents) {
       setNoticeDetail(data.data.contents);
-      if(noticeDetail) {
+      if (noticeDetail) {
         const currentDetail = noticeDetail;
         //제목 및 내용 설정
         const textArea = tRef1.current;
-        if(textArea) {
+        if (textArea) {
           textArea.value = noticeDetail.noticeCn; //내용 설정
         }
       }
     }
-  },[data,isSuccess]);
+  }, [data, isSuccess]);
 
   //공지사항이 업데이트된 후 제목 및 내용을 설정
   useEffect(() => {
-    if(noticeDetail) {
+    if (noticeDetail) {
       const currentDetail = noticeDetail;
 
       const textArea = tRef1.current;
-      if(textArea){
+      if (textArea) {
         textArea.value = currentDetail.noticeCn;
       }
-    }
-  },[noticeDetail]);
-
-  useEffect(() => {
-    if (noticeDetail) {
-      console.log("noticeDetail.noticeSj:", noticeDetail.noticeSj);
-    } else {
-      console.log("noticeDetail 비어있습니다.");
     }
   }, [noticeDetail]);
 
@@ -76,7 +67,9 @@ export default function NoticeDetail() {
 
     // 현재 날짜를 저장
     const today = new Date().toISOString().split("T")[0];
-    const hiddenPopups = JSON.parse(localStorage.getItem("hiddenPopups") || "{}");
+    const hiddenPopups = JSON.parse(
+      localStorage.getItem("hiddenPopups") || "{}"
+    );
 
     // 해당 공지사항 ID를 오늘 날짜와 함께 저장
     hiddenPopups[id] = today;
@@ -89,23 +82,43 @@ export default function NoticeDetail() {
   return (
     <>
       <Stack bgcolor={"white"} width={"100%"} height={"100%"}>
-        <Stack direction={"row"} padding={1} width={"100%"} height={"5%"} gap={2} alignItems={"center"} marginTop={1}>
+        <Stack
+          direction={"row"}
+          padding={1}
+          width={"100%"}
+          height={"5%"}
+          gap={2}
+          alignItems={"center"}
+          marginTop={1}
+        >
           <Typography>제목</Typography>
           <Box>
-            <BasicInput sx={{ width: "650px" }} value={noticeDetail ? noticeDetail.noticeSj : ""} readOnly></BasicInput>
+            <BasicInput
+              sx={{ width: "650px" }}
+              value={noticeDetail ? noticeDetail.noticeSj : ""}
+              readOnly
+            ></BasicInput>
           </Box>
         </Stack>
         <Box width={"98%"} height={"90%"} margin={1}>
-          <TextArea
-            height="400px"
-            resize="none"
-            ref={tRef1}
-            placeholder=""
-          />
+          <TextArea height="400px" resize="none" ref={tRef1} placeholder="" />
         </Box>
-        <Stack justifyContent={"space-between"} width={"100%"} direction={"row"} alignItems={"center"}>
-          <Stack direction={"row"} padding={1} alignItems={"center"} width={"80%"} gap={1}>
-            <BasicButton onClick={handleHideForADay}>하루동안 보이지않기</BasicButton>
+        <Stack
+          justifyContent={"space-between"}
+          width={"100%"}
+          direction={"row"}
+          alignItems={"center"}
+        >
+          <Stack
+            direction={"row"}
+            padding={1}
+            alignItems={"center"}
+            width={"80%"}
+            gap={1}
+          >
+            <BasicButton onClick={handleHideForADay}>
+              하루동안 보이지않기
+            </BasicButton>
           </Stack>
           <Stack direction={"row"} gap={1} margin={1}>
             <BasicButton onClick={() => window.close()}>닫기</BasicButton>
