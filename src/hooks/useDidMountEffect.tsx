aad080dcfeaffc9a14fce710eefbce57755ae495
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-01-08 23:52:06 +0900

    새로고침 에러 수정

---
+++ src/hooks/useDidMountEffect.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef, DependencyList } from "react";
+
+const useDidMountEffect = (func: () => void, deps: DependencyList) => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (didMount.current) {
+      func();
+    } else {
+      didMount.current = true;
+    }
+  }, deps);
+};
+
+export default useDidMountEffect;
