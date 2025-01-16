10116b782949326ef2666ff0aa89bafbf031bbbd
sonjiyeon <son.ji.dev@gmail.com>
2025-01-16 14:55:53 +0900

    사용자관리 api 추가 (팝업 추후 수정)

--- src/pages/SysManagement/AuthCode/index.tsx
+++ src/pages/SysManagement/AuthCode/index.tsx
@@ -8,47 +8,50 @@ import { tableTestData } from "../../../utils/testData";
 import { useSingleRowSelection } from "../../../hooks/useSingleRowSelection";
 import { Pagination } from "../../../components/Pagination";
 import { usePagination } from "../../../hooks/usePagination";
+import React from "react";
 
 export default function AuthCode() {
 
   // BasicTable에 연결할 한 행만 선택 가능하게 하는거(BasicTable 수정을 해야겐네요..)
   const { selectedRow, toggleRowSelection } = useSingleRowSelection();
 
-  const { currentPage, onChangePage } = usePagination();
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const onChangePage = (event: React.ChangeEvent<unknown>, newPage: number) => {
+    setCurrentPage(newPage);
+    console.log(`현재 페이지: ${newPage}`); // 콘솔에 현재 페이지 출력
+  };
 
   return (
     <>
-      <Stack width={"100%"} height={"100%"} marginBottom={1}>
-        <GrayBox>
+      <Stack width={"100%"} height={"100%"} gap={1}>
+        <GrayBox width={"100%"} height={"5%"}>
           <SearchInput placeholder="입력창"></SearchInput>
         </GrayBox>
-        <Stack height={"98"} gap={1} width={"100%"} justifyContent={"space-between"}>
-          <TableBox height="95%">
-            <TableBox.Inner>
-              <BasicTable data={tableTestData}>
-                <BasicTable.Th>이름</BasicTable.Th>
-                <BasicTable.Th>상담전화</BasicTable.Th>
-                <BasicTable.Th>상담일시</BasicTable.Th>
-                <BasicTable.Tbody>
-                  {tableTestData.map((item, index) => {
-                    return (
-                      <BasicTable.Tr
-                        key={index}
-                        isClicked={selectedRow.has(item.id)}
-                        onClick={() => toggleRowSelection(item.id)}
-                      >
-                        <BasicTable.Td>{item.name}</BasicTable.Td>
-                        <BasicTable.Td>{item.age}</BasicTable.Td>
-                        <BasicTable.Td>{item.job}</BasicTable.Td>
-                      </BasicTable.Tr>
-                    );
-                  })}
-                </BasicTable.Tbody>
-              </BasicTable>
-            </TableBox.Inner>
-            
-          </TableBox>
-          <Pagination count={25} page={currentPage} onChange={onChangePage}/>
+        <Stack width={"100%"} height={"95%"} gap={1}>
+          <TableBox.Inner>
+            <BasicTable data={tableTestData}>
+              <BasicTable.Th>발신전화번호</BasicTable.Th>
+              <BasicTable.Th>상담전화</BasicTable.Th>
+              <BasicTable.Th>상담일시</BasicTable.Th>
+              <BasicTable.Tbody>
+                {tableTestData.map((item, index) => {
+                  return (
+                    <BasicTable.Tr
+                      key={index}
+                      isClicked={selectedRow.has(item.id)}
+                      onClick={() => toggleRowSelection(item.id)}
+                    >
+                      <BasicTable.Td>{item.name}</BasicTable.Td>
+                      <BasicTable.Td>{item.age}</BasicTable.Td>
+                      <BasicTable.Td>{item.job}</BasicTable.Td>
+                    </BasicTable.Tr>
+                  );
+                })}
+              </BasicTable.Tbody>
+            </BasicTable>
+          </TableBox.Inner>
+          <Pagination count={25} page={currentPage} onChange={onChangePage} />
         </Stack>
       </Stack>
     </>
--- src/pages/SysManagement/User/index.tsx
+++ src/pages/SysManagement/User/index.tsx
@@ -9,7 +9,7 @@ import { Select } from "../../../components/Select";
 import { selectTestData, tableTestData } from "../../../utils/testData";
 import useSelect from "../../../hooks/useSelect";
 import CustomerInfo from "../../CustomerManagement/Registration/CustomerInfo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PathConstants from "../../../routers/path";
 import { openPopup } from "../../../utils/openPopup";
 import TableBox from "../../../components/Box/TableBox";
@@ -20,9 +20,101 @@ import TableSelect from "../../../components/Select/TableSelect";
 import { RiDeleteBinLine } from "react-icons/ri";
 import CheckboxTable from "../../../components/Table/CheckboxTable";
 import { useMultiRowSelection } from "../../../hooks/useMultiRowSelection";
-import { BiChevronLeft } from "react-icons/bi";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import api from "../../../api";
+import { UserListType } from "../../../api/userList";
+import { useAuthStore } from "../../../stores/authStore";
+import useModal from "../../../hooks/useModal";
+import { UserPermitSolutionType, useUserPermitSolution } from "../../../api/userPermissionSolution";
+import { userSolutionCount } from "../../../api/userSolutionCount";
+import { UserNonPermissionSolutionType, useUserNonPermissionSolution } from "../../../api/UserNonpermitSolution";
+import { userPermissionRegistration } from "../../../api/userPermissionRegistration";
+import { userNonPermissionRegistration } from "../../../api/userNonPermissionRegistration";
 
 export default function Registration() {
+
+  const [searchQuery, setSearchQuery] = useState(""); // 검색어 상태 관리
+  const [searchInput, setSearchInput] = useState(""); // 검색 입력값 상태 관리
+
+  //사용자 목록 가져오기
+  const { isSuccess, data } = api.UserList.useUserList(searchQuery);
+  const [userList, setUserList] = useState<UserListType[]>([]);
+
+  //사용자 허가 솔루션 목록 가져오기
+  const [userSelectRow, setSelectRow] = useState("");  //사용자 허가 솔루션을 가져오기위한 id
+  const [userPermitSolution, setUserPermitSolution] = useState<UserPermitSolutionType[]>([]);
+  const { data: userPermitSolutionData, isLoading: isLoadingPermitSolution } = useUserPermitSolution(userSelectRow);
+  const [solutionCounts, setSolutionCounts] = useState<Record<string, string>>({}); // 각 행의 solutionCount 관리
+  const [userNo, setUserNo] = useState("");
+  const [solutionId, setSolutionId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [solutionTotalCount, setSolutionTotalCount] = useState("");
+
+  //사용자 미허가 솔루션 목록 가져오기
+  const [userNonPermitSolution, setUserNonPermitSolution] = useState<UserNonPermissionSolutionType[]>([]);
+  const { data: userNonPermitSolutionData, isLoading: isLoadingNonPermitSolution } = useUserNonPermissionSolution(userSelectRow);
+
+  const permissionRegistration = userPermissionRegistration();
+  const nonPermissionRegistration = userNonPermissionRegistration();
+
+
+  //데이터 정리
+  const userListData = userList.map((item) => ({
+    userNo: item.userNo,
+    userNm: item.userNm,
+    attlistMbtlNo: item.attlistMbtlNo,
+    loginIdPrefix: item.loginIdPrefix,
+    loginId: item.loginId,
+    cmpnm: item.cmpnm,
+    bizrno: item.bizrno,
+    rprsntvNm: item.rprsntvNm,
+    adres1: item.adres1,
+    adres2: item.adres2,
+    reprsntTelno: item.reprsntTelno,
+    useYn: item.useYn,
+  }));
+
+  const formatNonPermissionList = userNonPermitSolution.map((item) => ({
+    id: item.slutnId,               // ID
+    slutnId: item.slutnId,          // 솔루션 ID
+    slutnNm: item.slutnNm,          // 솔루션 이름
+    lisneSeCd: item.lisneSeCd,      // 라이센스 코드
+    lisneSeNm: item.lisneSeNm,      // 라이센스 이름
+    userNo: item.userNo,            // 사용자 번호
+    isOk: item.isOk,                // 사용 여부
+  }));
+
+  const formatPermissionList = userPermitSolution.map((item) => ({
+    id: item.slutnId,               // ID
+    slutnId: item.slutnId,          // 솔루션 ID
+    slutnNm: item.slutnNm,          // 솔루션 이름
+    lisneSeCd: item.lisneSeCd,      // 라이센스 코드
+    lisneSeNm: item.lisneSeNm,      // 라이센스 이름
+    userNo: item.userNo,            // 사용자 번호
+    chrgcnt: item.chrgcnt,                // 사용 여부
+    sptlisneCnt: item.sptlisneCnt,                // 사용 여부
+    userlisneCnt: item.userlisneCnt,                // 사용 여부
+  }));
+
+  //솔루션 전체 갯수를 수정할 경우 필요한 데이터
+  const solutionTotalData = {
+    body: {
+      userNo: userNo,
+      slutnId: solutionId,
+      userlisneCnt: solutionTotalCount,
+      userId: userId,
+    }
+  };
+
+  //api 호출을 위한 id호출
+  const { loginId } = useAuthStore(["loginId"]);
+  const { mutate: userSolutionCountAPI } = userSolutionCount(solutionTotalData);
+
+  //모달
+  const { openModal, closeModal } = useModal();
+
+
+
   const { selectListData, selectValue, handleChange } = useSelect(
     selectTestData,
     "value",
@@ -53,15 +145,194 @@ export default function Registration() {
   const uploadUser = {
     url: PathConstants.System.UserUpload,
     windowName: "사용자 등록 및 수정",
-    windowFeatures: "width=500,height=500,scrollbars=yes,resizable=yes",
+    windowFeatures: "width=700,height=700,scrollbars=yes,resizable=yes",
   };
+
+  //이벤트 발생시 동작
+
+  useEffect(() => {
+    if (data?.data.contents) {
+      setUserList(data.data.contents);
+      //console.log("사용자 데이터 확인 : ", data.data.contents);
+    }
+  }, [data, isSuccess, searchQuery]);
+
+  const handleSearch = () => {
+    setSearchQuery(searchInput); // 검색어 업데이트
+    //console.log("검색 실행:", searchQuery);
+    // 검색 로직 추가 (API 호출)
+  };
+
+  useEffect(() => {
+
+    if (userPermitSolutionData?.data.contents) {
+      setUserPermitSolution(userPermitSolutionData.data.contents);
+
+      //console.log("선택된 id : ", userSelectRow);
+      //console.log("그리고 나온 데이터 : ", userPermitSolutionData)
+    }
+  }, [userPermitSolutionData, userSelectRow]);
+
+  useEffect(() => {
+    if (userNonPermitSolutionData?.data.contents) {
+      setUserNonPermitSolution(userNonPermitSolutionData.data.contents);
+    }
+  }, [userSelectRow, userNonPermitSolutionData]);
+
+  // 각 입력값 변경 핸들러
+  const handleSolutionCountChange = (slutnId: string, value: string) => {
+    setSolutionCounts((prev) => ({
+      ...prev,
+      [slutnId]: value, // slutnId를 키로 사용하여 각 행의 값 저장
+    }));
+  };
+
+  // 조회 버튼 클릭 시 데이터 출력
+  const handleCheckSolutionData = () => {
+    const updatedData = userPermitSolution.map((item) => {
+      const updatedCount = solutionCounts[item.slutnId] || ""; // 변경된 값이 있으면 가져옴
+      return {
+        ...item,
+        updatedCount,
+      };
+    });
+
+    updatedData.forEach((item) => {
+      if (item.updatedCount) {
+        console.log("변경된 데이터:");
+        console.log("slutnId:", item.slutnId);
+        console.log("userNo:", userSelectRow); // 선택된 사용자 ID
+        console.log("userlisneCnt:", item.updatedCount);
+        console.log("userId:", item.userId); // item에 userID 포함 여부 확인 필요
+
+        const solutionTotalData = {
+          body: {
+            userNo: userSelectRow,
+            slutnId: item.slutnId,
+            userlisneCnt: item.updatedCount,
+            userId: loginId,
+          },
+        };
+
+        console.log("body:", solutionTotalData);
+
+        userSolutionCountAPI(solutionTotalData, {
+          onSuccess: (response) => {
+            console.log("API 호출 성공:", response);
+            // 성공 처리 로직
+            if (response.data.message === "SUCCESS") {
+              //window.location.reload();
+              console.log("response", response.data)
+            }
+          },
+          onError: (error) => {
+            console.error("API 호출 실패:", error);
+            // 에러 처리 로직 추가
+          },
+        });
+      }
+    });
+  };
+
+  const handleSubmit = () => {
+    // 선택된 데이터 추출 및 변환
+    const userList = Array.from(unauthorizedSelectedRows).map((rowId) => {
+      const selectedItem = userNonPermitSolution.find(item => item.slutnId === rowId);
+      return {
+        slutnId: selectedItem?.slutnId || '',
+        userNo: userSelectRow, // 필요한 경우 API에서 userNo 데이터 확인
+        lisneCnt: "1",
+        useYn: "",
+        delYn: "",
+        rmk: "",
+        userId: ""
+      };
+    });
+
+    // API 호출 데이터 생성
+    const requestData = {
+      userList,
+      userId: "", // 현재 사용자 ID (필요시 추가 로직 구현)
+    };
+
+    // 생성된 데이터 콘솔 출력
+    console.log('API 호출 전에 생성된 데이터:', requestData);
+
+    // API 호출
+    permissionRegistration.mutate(
+      { body: requestData },
+      {
+        onSuccess: (response) => {
+          console.log("이동성공");
+          if (response.data.result === "SUCCESS") {
+            console.log("response.data", response.data);
+            window.location.reload();
+          }
+        },
+        onError: (error) => {
+          console.error("이동 실패:", error);
+        },
+      }
+    )
+  };
+
+  const handleNonPerSubmit = () => {
+    const userList = Array.from(authorizedSelectedRows).map((rowId) => {
+      const selectedItem = userPermitSolution.find(item => item.slutnId === rowId);
+      return {
+        slutnId: selectedItem?.slutnId || '',
+        userNo: userSelectRow, // 필요한 경우 API에서 userNo 데이터 확인
+        lisneCnt: "1",
+        useYn: "",
+        delYn: "",
+        rmk: "",
+        userId: ""
+      };
+    });
+
+    // API 호출 데이터 생성
+    const requestData = {
+      userList,
+      userId: "", // 현재 사용자 ID (필요시 추가 로직 구현)
+    };
+
+    // 생성된 데이터 콘솔 출력
+    console.log('API 호출 전에 생성된 데이터:', requestData);
+
+    // API 호출
+    nonPermissionRegistration.mutate(
+      { body: requestData },
+      {
+        onSuccess: (response) => {
+          console.log("이동성공");
+          if (response.data.result === "SUCCESS") {
+            console.log("response.data", response.data);
+            window.location.reload();
+          }
+        },
+        onError: (error) => {
+          console.error("이동 실패:", error);
+        },
+      }
+    )
+  };
+
 
   return (
     <>
       {/* 사용자 리스트 테이블 - 상단 테이블 */}
       <Stack width={"100%"} height={"100%"} gap={1}>
         <GrayBox gap={1} justifyContent={"space-between"}>
-          <SearchInput placeholder="사용자이름 검색" />
+          <SearchInput
+            placeholder="사용자이름 검색"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)} // 검색어 입력값 업데이트
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleSearch(); // 검색 실행 함수 호출
+              }
+            }}
+          />
           <Box gap={1}>
             <BasicButton
               onClick={() => {
@@ -79,8 +350,8 @@ export default function Registration() {
         <Stack width={"100%"} height={"95%"}>
           <Stack width={"100%"} height={"40%"}>
             <TableBox.Inner>
-              <BasicTable data={tableTestData}>
-                <BasicTable.Th>사용자ID</BasicTable.Th>
+              <BasicTable data={userListData}>
+                <BasicTable.Th>사용자번호</BasicTable.Th>
                 <BasicTable.Th>사용자이름</BasicTable.Th>
                 <BasicTable.Th>휴대전화</BasicTable.Th>
                 <BasicTable.Th>PREFIX</BasicTable.Th>
@@ -90,20 +361,30 @@ export default function Registration() {
                 <BasicTable.Th>삭제</BasicTable.Th>
                 <BasicTable.Th>수정</BasicTable.Th>
                 <BasicTable.Tbody>
-                  {tableTestData.map((item, index) => {
+                  {userListData.map((item, index) => {
                     return (
                       <BasicTable.Tr
                         key={index}
-                        isClicked={selectedRow.has(item.id)}
-                        onClick={() => toggleRowSelection(item.id)}
+                        isClicked={userSelectRow === item.userNo}
+                        onClick={() => {
+                          if (userSelectRow === item.userNo) {
+                            // 동일 행을 클릭하면 선택 해제
+                            setSelectRow("");
+                            console.log("선택 해제");
+                          } else {
+                            // 다른 행 클릭 시 상태 업데이트
+                            setSelectRow(item.userNo);
+                            console.log("선택된 사용자 ID:", item.userNo);
+                          }
+                        }}
                       >
-                        <BasicTable.Td>{item.phone}</BasicTable.Td>
-                        <BasicTable.Td>{item.name}</BasicTable.Td>
-                        <BasicTable.Td>{item.age}</BasicTable.Td>
-                        <BasicTable.Td>{item.job}</BasicTable.Td>
-                        <BasicTable.Td>{item.job}</BasicTable.Td>
-                        <BasicTable.Td>{item.job}</BasicTable.Td>
-                        <BasicTable.Td>{item.job}</BasicTable.Td>
+                        <BasicTable.Td>{item.userNo}</BasicTable.Td>
+                        <BasicTable.Td>{item.userNm}</BasicTable.Td>
+                        <BasicTable.Td>{item.attlistMbtlNo}</BasicTable.Td>
+                        <BasicTable.Td>{item.loginIdPrefix}</BasicTable.Td>
+                        <BasicTable.Td>{item.cmpnm}</BasicTable.Td>
+                        <BasicTable.Td>{item.bizrno}</BasicTable.Td>
+                        <BasicTable.Td>{item.useYn}</BasicTable.Td>
                         <BasicTable.Td>
                           <IconButton>
                             <RiDeleteBinLine color="#f4475f" />
@@ -113,7 +394,7 @@ export default function Registration() {
                           <BasicButton
                             onClick={() => {
                               openPopup({
-                                url: uploadUser.url,
+                                url: `${uploadUser.url}?id=${item.userNo}`,
                                 windowName: uploadUser.windowName,
                                 windowFeatures: uploadUser.windowFeatures,
                               });
@@ -137,7 +418,7 @@ export default function Registration() {
                 </GrayBox>
                 <TableBox.Inner>
                   <CheckboxTable
-                    data={tableTestData}
+                    data={formatPermissionList}
                     selectedRows={authorizedSelectedRows}
                     toggleRowsSelection={toggleAuthorizedRowsSelection}
                   >
@@ -162,22 +443,31 @@ export default function Registration() {
                       </CheckboxTable.Tr>
                     </CheckboxTable.Thead>
                     <CheckboxTable.Tbody>
-                      {tableTestData.map((item) => (
-                        <CheckboxTable.Tr key={item.id} id={item.id}>
+                      {formatPermissionList.map((item) => (
+                        <CheckboxTable.Tr key={item.slutnId} id={item.slutnId}>
                           <CheckboxTable.CheckboxTd item={item} />
-                          <CheckboxTable.Td>{item.name}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.phone}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.job}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.address}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.age}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.age}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.slutnId}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.slutnNm}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.lisneSeNm}</CheckboxTable.Td>
+                          <CheckboxTable.Td>
+                            <BasicInput
+                              placeholder={item.userlisneCnt}
+                              value={solutionCounts[item.slutnId] || ""} // 현재 행의 입력값
+                              onChange={(e) =>
+                                handleSolutionCountChange(item.slutnId, e.target.value)
+                              } // 각 행의 상태 업데이트
+                            />
+                          </CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.sptlisneCnt}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.chrgcnt}</CheckboxTable.Td>
                         </CheckboxTable.Tr>
                       ))}
                     </CheckboxTable.Tbody>
                   </CheckboxTable>
                 </TableBox.Inner>
-                <GrayBox>
-                  <BasicButton sx={{ marginLeft: "auto" }}>새로고침</BasicButton>
+                <GrayBox justifyContent={"end"} gap={1}>
+                  <BasicButton onClick={handleCheckSolutionData}>적용</BasicButton>
+                  <BasicButton>새로고침</BasicButton>
                 </GrayBox>
               </Stack>
               <Stack width={"2%"} bgcolor={"white"} justifyContent={"space-between"}>
@@ -190,6 +480,7 @@ export default function Registration() {
                     margin: "0",
                     minWidth: "unset", // 기본 minWidth 해제
                   }}
+                  onClick={handleSubmit}
                 >
                   <BiChevronLeft size={"24px"} />
                 </BasicButton>
@@ -202,8 +493,9 @@ export default function Registration() {
                     margin: "0",
                     minWidth: "unset", // 기본 minWidth 해제
                   }}
+                  onClick={handleNonPerSubmit}
                 >
-                  <BiChevronLeft size={"24px"} />
+                  <BiChevronRight size={"24px"} />
                 </BasicButton>
               </Stack>
               <Stack width={"29%"} height={"100%"} gap={1}>
@@ -212,7 +504,7 @@ export default function Registration() {
                 </GrayBox>
                 <TableBox.Inner>
                   <CheckboxTable
-                    data={tableTestData}
+                    data={formatNonPermissionList}
                     selectedRows={unauthorizedSelectedRows}
                     toggleRowsSelection={toggleUnauthorizedRowsSelection}
                   >
@@ -222,16 +514,18 @@ export default function Registration() {
                         <CheckboxTable.Th>솔루션ID</CheckboxTable.Th>
                         <CheckboxTable.Th>솔루션이름</CheckboxTable.Th>
                         <CheckboxTable.Th>구분</CheckboxTable.Th>
+                        <CheckboxTable.Th>사용여부</CheckboxTable.Th>
                       </CheckboxTable.Tr>
                     </CheckboxTable.Thead>
 
                     <CheckboxTable.Tbody>
-                      {tableTestData.map((item) => (
-                        <CheckboxTable.Tr key={item.id} id={item.id}>
+                      {formatNonPermissionList.map((item) => (
+                        <CheckboxTable.Tr key={item.slutnId} id={item.slutnId}>
                           <CheckboxTable.CheckboxTd item={item} />
-                          <CheckboxTable.Td>{item.name}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.phone}</CheckboxTable.Td>
-                          <CheckboxTable.Td>{item.job}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.slutnId}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.slutnNm}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.lisneSeNm}</CheckboxTable.Td>
+                          <CheckboxTable.Td>{item.isOk}</CheckboxTable.Td>
                         </CheckboxTable.Tr>
                       ))}
                     </CheckboxTable.Tbody>
