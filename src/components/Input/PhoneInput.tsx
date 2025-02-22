5fe1edae6e66f8e0fc043c29f8dbccd534e10cab
sonjiyeon <son.ji.dev@gmail.com>
2025-02-22 13:12:28 +0900

    phoneInput 수정

--- src/components/Input/PhoneInput.tsx
+++ src/components/Input/PhoneInput.tsx
@@ -1,72 +1,68 @@
 import { forwardRef, useState } from "react";
 import { OutlinedInput, OutlinedInputProps } from "@mui/material";
+import { Controller } from "react-hook-form";
 
 interface InputProps extends OutlinedInputProps {
+  control: any;
+  name: string;
   placeholder?: string;
-  value?: string;
 }
 
 //전화번호 정규식 추가 inputbox
+//사용자가 입력한 값을 PhoneInput에 넘기고 입력값을 수정한 뒤 보내야하기때문에 Controller를 사용하도록 수정
 const PhoneInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ placeholder = "", value, defaultValue = "", ...rest }, ref) => {
-    const [phoneNum, setPhoneNum] = useState(value);
-
-    // 숫자만 입력되도록 함
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const numericValue = event.target.value.replace(/\D/g, ""); // 숫자만 남기기
-
-      let formattedValue = numericValue;
-
-      if (numericValue.length === 8) {
-        // 8자리 업체번호 1668-0000
-        formattedValue = numericValue.replace(/(\d{4})(\d{4})/, "$1-$2");
-      } else if (numericValue.startsWith("02")) {
-        //02로 시작하는 전화번호
-        formattedValue = numericValue.replace(
-          /^(02)(\d{3,4})(\d{4})$/,
-          "$1-$2-$3"
-        );
-      } else if (numericValue.length === 11) {
-        // 11자리 휴대폰 번호
-        formattedValue = numericValue.replace(
-          /(\d{3})(\d{4})(\d{4})/,
-          "$1-$2-$3"
-        );
-      } else if (numericValue.startsWith("080")) {
-        //080으로 시작하며 중간자리가 3자리인 전화번호
-        formattedValue = numericValue.replace(
-          /^(080)(\d{3})(\d{4})$/,
-          "$1-$2-$3"
-        );
-      } else if (numericValue.length === 10) {
-        //10자리 서울 외 전화번호
-        formattedValue = numericValue.replace(
-          /(\d{3})(\d{3})(\d{4})/,
-          "$1-$2-$3"
-        );
-      }
-
-      setPhoneNum(formattedValue);
-    };
-
-    // 포커스 아웃 시 값 초기화
-    const handleBlur = () => {
-      setPhoneNum(value); // 입력값 초기화
-    };
-
+  ({ control, name, placeholder = "", ...rest }, ref) => {
     return (
-      <OutlinedInput
-        {...rest}
-        size="small"
-        placeholder={placeholder}
-        inputRef={ref}
-        value={phoneNum}
-        defaultValue={defaultValue}
-        onChange={handleChange} // 숫자만 입력받도록 설정
-        onBlur={handleBlur} // 포커스 아웃 시 초기화
-        sx={{
-          backgroundColor: "white",
-          ...(rest.sx || {}),
+      <Controller
+        name={name}  //필드명
+        control={control}  //Controller를 사용하기위해 control전달함
+        render={({ field: { onChange, value } }) => {
+          // 숫자만 입력되도록 변경
+          const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const numericValue = event.target.value.replace(/\D/g, "");
+            let formattedValue = numericValue;
+
+            if (numericValue.length === 8) {
+              formattedValue = numericValue.replace(/(\d{4})(\d{4})/, "$1-$2");
+            } else if (numericValue.startsWith("02")) {
+              formattedValue = numericValue.replace(
+                /^(02)(\d{3,4})(\d{4})$/,
+                "$1-$2-$3"
+              );
+            } else if (numericValue.length === 11) {
+              formattedValue = numericValue.replace(
+                /(\d{3})(\d{4})(\d{4})/,
+                "$1-$2-$3"
+              );
+            } else if (numericValue.startsWith("080")) {
+              formattedValue = numericValue.replace(
+                /^(080)(\d{3})(\d{4})$/,
+                "$1-$2-$3"
+              );
+            } else if (numericValue.length === 10) {
+              formattedValue = numericValue.replace(
+                /(\d{3})(\d{3})(\d{4})/,
+                "$1-$2-$3"
+              );
+            }
+
+            onChange(formattedValue);
+          };
+
+          return (
+            <OutlinedInput
+              {...rest}
+              size="small"
+              placeholder={placeholder}
+              inputRef={ref}
+              value={value || ""}
+              onChange={handleChange}
+              sx={{
+                backgroundColor: "white",
+                ...(rest.sx || {}),
+              }}
+            />
+          );
         }}
       />
     );
