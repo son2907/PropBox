2b0be5478853995ec1afbcd0ec4bd1137ece55d6
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-03-20 00:11:15 +0900

    targetId 추가

--- src/pages/CalManagement/Consultation/popup/RegisterSenerNumber.tsx
+++ src/pages/CalManagement/Consultation/popup/RegisterSenerNumber.tsx
@@ -171,7 +171,7 @@ export default function RegisterSenerNumber() {
       console.log("opCert결과:", res);
       const mdlTkn = res?.data?.data.contents.mdlTkn;
       const rsltCd = res?.data?.data.contents.rsltCd;
-      const popupUrl = `https://safe.ok-name.co.kr/CommonSvl?tc=kcb.oknm.online.safehscert.popup.cmd.P931_CertChoiceCmd&cp_cd=${rsltCd}&mdl_tkn=${mdlTkn}`;
+      const popupUrl = `https://safe.ok-name.co.kr/CommonSvl?tc=kcb.oknm.online.safehscert.popup.cmd.P931_CertChoiceCmd&cp_cd=${rsltCd}&mdl_tkn=${mdlTkn}&target_id=`;
       window.open(popupUrl, "okcertPopup", "width=500,height=600");
     });
   };
