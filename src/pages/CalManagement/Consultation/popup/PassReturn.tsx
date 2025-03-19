b1e548d3f84c249a31f97945f0f28b1ebc9db353
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-03-20 05:05:27 +0900

    PassReturnPage 수정중..

--- src/pages/CalManagement/Consultation/popup/PassReturn.tsx
+++ src/pages/CalManagement/Consultation/popup/PassReturn.tsx
@@ -3,43 +3,37 @@ import { useSearchParams } from "react-router-dom";
 import { useOkcertResult } from "../../../../api/crtfc";
 
 const PassReturnPage = () => {
-  const [mdlTkn, setMdlTkn] = useState<string>("");
   const [searchParams] = useSearchParams();
-  const { refetch } = useOkcertResult({ mdlTkn });
+  const { data, refetch } = useOkcertResult({
+    mdlTkn: searchParams.get("mdl_tkn"),
+  });
+  console.log("결과:", data);
 
-  useEffect(() => {
-    const mdlTkn = searchParams.get("mdl_tkn");
-    setMdlTkn(mdlTkn || "");
+  //   useEffect(() => {
 
-    if (!mdlTkn) {
-      return;
-    }
+  //     // const fetchData = async () => {
+  //     //   try {
+  //     //     if (data?.data.contents.rsltCd === "T993") {
+  //     //       localStorage.setItem("authData", JSON.stringify(data?.data.contents));
+  //     //       window.opener?.postMessage(
+  //     //         { type: "AUTH_SUCCESS", data: "SUCCESS" },
+  //     //         "*"
+  //     //       );
+  //     //     } else {
+  //     //       window.opener?.postMessage(
+  //     //         { type: "AUTH_FAILURE", data: "FALSE" },
+  //     //         "*"
+  //     //       );
+  //     //     }
+  //     //     // window.close();
+  //     //   } catch (err) {
+  //     //     console.error("에러 발생:", err);
+  //     //   }
+  //     // };
 
-    const fetchData = async () => {
-      try {
-        const { data } = await refetch();
-
-        if (data?.data.contents.rsltCd === "T993") {
-          localStorage.setItem("authData", JSON.stringify(data?.data.contents));
-          window.opener?.postMessage(
-            { type: "AUTH_SUCCESS", data: "SUCCESS" },
-            "*"
-          );
-        } else {
-          window.opener?.postMessage(
-            { type: "AUTH_FAILURE", data: "FALSE" },
-            "*"
-          );
-        }
-        // window.close();
-      } catch (err) {
-        console.error("에러 발생:", err);
-      }
-    };
-
-    fetchData();
-  }, [searchParams, refetch]);
-  return <></>;
+  //     fetchData();
+  //   }, []);
+  return <div>테스트</div>;
 };
 
 export default PassReturnPage;
