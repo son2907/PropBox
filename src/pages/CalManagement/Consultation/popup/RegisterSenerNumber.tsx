ffd233fd8f7e42240759035fd5647d7763d79925
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-03-20 00:44:50 +0900

    팝업 크기 지정..

--- src/pages/CalManagement/Consultation/popup/RegisterSenerNumber.tsx
+++ src/pages/CalManagement/Consultation/popup/RegisterSenerNumber.tsx
@@ -188,7 +188,7 @@ export default function RegisterSenerNumber() {
       const result = await response.json();
       console.log(result);
       const popupUrl = `https://safe.ok-name.co.kr/CommonSvl?tc=kcb.oknm.online.safehscert.popup.cmd.P931_CertChoiceCmd&cp_cd=${cp_CD}&mdl_tkn=${mdl_tkn}&target_id=`;
-      window.open(popupUrl, "okcertPopup");
+      window.open(popupUrl, "okcertPopup", "width=500,height=600");
     } catch (error) {
       console.error("Error:", error);
     }
