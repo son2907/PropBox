965f39e028af513a418d34280be298e382e24bbf
ain.jo.dev@gmail.com <ainjo1117@ciel.co.kr>
2025-03-26 01:03:03 +0900

    사용하지 않는 유틸 함수 제거 및 Date로 YYYY-MM-DD HH:MM:SS 설정하는 formating 함수 생성

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -6,3 +6,15 @@ export function getFormattedDate(date?: Date | undefined) {
 
   return `${year}${month}${day}`;
 }
+
+export function getFormattedDateTime(date?: Date | undefined) {
+  const today = date ?? new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1
+  const day = String(today.getDate()).padStart(2, "0");
+  const hours = String(today.getHours()).padStart(2, "0");
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  const seconds = String(today.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
